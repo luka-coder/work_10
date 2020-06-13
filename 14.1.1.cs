80b97cb68e62c7bb84ca926b93df37c0febe9f72
luka <55693980+luka-coder@users.noreply.github.com>
2020-06-13 17:57:30 +0400

    my commit

---
+++ 14.1.1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Lstr = new List<string> { "Armada", "Majesty", "Salomon", "Rossignol", "Fischer", "Spyder", "Line", "NISSAN", "Icelantic", "K2" };
+            Lstr.Add("AddSkiBrands");
+            Lstr.Add("AddSkiBrands");
+
+            Console.WriteLine($"Array Size: {Lstr.Count}\n");
+            Console.WriteLine($"Array Complementaryi: \n");
+            Lstr.ForEach(a => Console.WriteLine(a));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 14.2.1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strList = new List<string> { "item", "item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9" };
+
+            Console.WriteLine($"First item: {strList[0]}");
+            Console.WriteLine($"List Size: {strList.Count}\nItems:\n");
+            strList.ForEach(s => Console.WriteLine(s));
+
+            strList.Clear();
+            Console.WriteLine($"\nItem Quantity after deleting: {strList.Count}");
+            Console.ReadKey();
+
+        }
+    }
+}
